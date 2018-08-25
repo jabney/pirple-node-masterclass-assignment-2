71e4c99560a8de04b7ea860572b070282178a895
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2018-08-25 10:41:06 -0400

    feat: add the config
    - I had to do the odd thing with exporting a function to be able to defer execution so that this module could be unit tested. That makes this module less obvious to use, so I'm not sure how I feel about it.

---
+++ test/config.spec.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+
+describe('config', () => {
+    const originalEnv = process.env;
+
+    const stubProcessEnv = (nodeEnv) => {
+        process.env = {
+            NODE_ENV: nodeEnv
+        };
+    };
+
+    afterEach(() => {
+        process.env = originalEnv;
+    });
+
+    it('should return the config for staging by default', () => {
+        stubProcessEnv('');
+
+        const config = require('../lib/config')();
+
+        assert.strictEqual(config.envName, 'staging');
+    });
+
+    it('should return the config for production', () => {
+        stubProcessEnv('PRODUCTION');
+
+        const config = require('../lib/config')();
+
+        assert.strictEqual(config.envName, 'production');
+    });
+});
